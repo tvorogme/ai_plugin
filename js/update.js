9fb9082fee125a7ffad8c1301ef46621bd891320
Andrey <tvorog@tvorog.me>
2019-02-04 16:57:12 +0300

    Update

--- js/update.js
+++ js/update.js
@@ -7,21 +7,22 @@ const cep_extension_path = "C:\\Program Files (x86)\\Common Files\\Adobe\\CEP\\e
 const zip_path = cep_extension_path + "\\vkusvill.zip";
 const download_path = "https://codeload.github.com/tvorogme/ai_plugin/zip/master";
 
+const readStream = fs.createReadStream(zip_path);
+const writeStream = fstream.Writer(cep_extension_path);
 
 function download() {
     try {
+        alert("Обновление в процессе, дождитесь перезагрузки плагина. Если плагин не обновляется - запустите приложение с правами администратора.");
         run('app.path', (result) => console.log(result));
         const file = fs.createWriteStream(zip_path);
         const request = https.get(download_path, (response) => {
             response.pipe(file);
 
             file.on('finish', function () {
-                const readStream = fs.createReadStream(zip_path);
-                const writeStream = fstream.Writer(cep_extension_path);
+                readStream.pipe(unzip.Parse()).pipe(writeStream);
 
-                readStream
-                    .pipe(unzip.Parse())
-                    .pipe(writeStream)
+                alert("Успешно обновили");
+                new CSInterface().requestOpenExtension('com.adobe.vkusvill.extension');
             });
         });
 
