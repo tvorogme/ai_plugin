fd45af97a45569a363841026376be33edbfe0104
Andrey <tvorog@tvorog.me>
2019-02-12 11:48:30 +0300

    Fix font by template

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+const https = require('https');
 const range = (start, stop, step) => {
     if (typeof stop == 'undefined') {
         // one param defined
@@ -21,7 +22,33 @@ const range = (start, stop, step) => {
     return result;
 };
 
-const words_to_fix = ['масса нетто', 'срок годности', 'масса нетто:', 'срок годности:', 'объем', 'объём', 'объем:', 'объём:'];
+// Скачать файл по ссылке
+const downlodFile = (url, func) => {
+    https.get(url).on('response', (response) => {
+        let body = '';
+
+        response.on('data', (chunk) => {
+            body += chunk;
+        });
+        response.on('end', () => func(body));
+    });
+};
+
+// Символы, которые нужно заменить в Regexp
+const regexFixChars = ['.', '^', '$', '+', '-', '?', '(', ')', '[', ']', '{', '}', '|'];
+
+// Заменить все вхождения в строке
+const replaceAll = (item, search, replace) => item.split(search).join(replace);
+
+// Преобразовать в регекс
+const toRegex = e => new RegExp(replaceAll(replaceAll(e, ' *', ' (\\S*){1}'), '* ', '(\\S*){1} '), 'g');
+
+// Перед преобразованием пофиксим похожие символы
+const fixBeforeRegex = (item) => [replaceAll(item, '\\', '\\\\'), ...regexFixChars].reduce(
+    (item, fixItem) => replaceAll(item, fixItem));
+
+const words_to_replace = {'lol': 'kek'};
+const words_to_replace_keys = Object.keys(words_to_replace).map(fixBeforeRegex).map(toRegex);
 
 function mapOverChars(textFrameIndex, toFixLength, charIndex, toUpper = true) {
     const textFrameTemplate = `app.activeDocument.textFrames[${textFrameIndex}].textRange`;
@@ -39,7 +66,12 @@ function mapOverChars(textFrameIndex, toFixLength, charIndex, toUpper = true) {
     return true
 }
 
+let words_to_fix = [];
+
 function fixFonts(e) {
+    downlodFile('https://raw.githubusercontent.com/tvorogme/ai_plugin/master/font_fix', (data) => {
+        words_to_fix = data.split('\n').slice(0, -1).map(fixBeforeRegex).map(toRegex);
+    });
 
     // достаем длину текст фреймов
     run('app.activeDocument.textFrames.length', (textRangeLength) => {
@@ -51,9 +83,11 @@ function fixFonts(e) {
             run(`app.activeDocument.textFrames[${textFrameIndex}].textRange.contents`, (text) => {
                 // сравним его с тем, который нужно преобразовать
                 const lowerText = text.toLowerCase();
+                const founded_words = [].concat.apply([],
+                    words_to_fix.map(regex => lowerText.match(regex)));
 
                 // для каждого слова
-                words_to_fix.map((textToFix) => {
+                founded_words.map((textToFix) => {
 
                     // если оно есть в тексте
                     if (lowerText.indexOf(textToFix) !== -1) {
@@ -61,18 +95,32 @@ function fixFonts(e) {
                         // пофиксим основное слово
                         mapOverChars(textFrameIndex, textToFix.length, lowerText.indexOf(textToFix));
 
-                        // Найдем где заканчивается последнее слово
-                        const main_word_end = lowerText.indexOf(textToFix) + textToFix.length;
-
-                        // Найдем где заканчивается предложение
-                        const text_end_on = lowerText.slice(
-                            lowerText.indexOf(textToFix) + textToFix.length).indexOf('.');
-
-                        // пофиксим все слова после основного слова
-                        mapOverChars(textFrameIndex,  text_end_on + 1, main_word_end)
-
+                        // // Найдем где заканчивается последнее слово
+                        // const main_word_end = lowerText.indexOf(textToFix) + textToFix.length;
+                        //
+                        // // Найдем где заканчивается предложение
+                        // const text_end_on = lowerText.slice(
+                        //     lowerText.indexOf(textToFix) + textToFix.length).indexOf('.');
+                        //
+                        // // пофиксим все слова после основного слова
+                        // mapOverChars(textFrameIndex, text_end_on + 1, main_word_end)
                     }
                 });
+
+                // const founded_words_to_replace = [].concat.apply([],
+                //     words_to_replace_keys.map(regex => lowerText.match(regex)));
+                //
+                // // для каждого слова
+                // founded_words_to_replace.map((textToReplace) => {
+                //
+                //     // если оно есть в тексте
+                //     if (lowerText.indexOf(textToReplace) !== -1) {
+                //
+                //         // пофиксим основное слово
+                //         mapOverChars(textFrameIndex, textToFix.length, lowerText.indexOf(textToFix));
+                //
+                //     }
+                // });
 
                 document.getElementById('status').innerHTML = 'Успешно';
 
@@ -82,3 +130,52 @@ function fixFonts(e) {
         })
     });
 }
+
+//
+// let founded = false;
+//
+// function stopCheck() {
+//     founded = true;
+//     document.getElementById('status').innerHTML = '100% Magenta';
+// }
+//
+// function checkForMagenta() {
+//     founded = false;
+//     document.getElementById('status').innerHTML = 'Начал проверку';
+//
+//     // достаем длину текст фреймов
+//     run('app.activeDocument.textFrames.length', (textRangeLength) => {
+//
+//         // для каждого фрейма
+//         range(parseInt(textRangeLength)).map((textFrameIndex) => {
+//
+//             // посмотрим на текст
+//             run(`app.activeDocument.textFrames[${textFrameIndex}].textRange.length`, (charsLength) => {
+//                 range(parseInt(charsLength)).map((charIndex) => {
+//
+//                     if (!founded) {
+//                         const curColor = `app.activeDocument.textFrames[${textFrameIndex}].characters[${charIndex}].characterAttributes.fillColor`;
+//
+//                         run(curColor, (color) => {
+//                             run(curColor + '.black', (e) => {
+//                                 e !== 'undefined' ?
+//                                     parseInt(e) === 0 ? run(curColor + '.cyan', (e) => e !== 'undefined' ?
+//                                         parseInt(e) === 0 ? run(curColor + '.yellow', (e) => e !== 'undefined' ? parseInt(e) === 0 ?
+//                                             run(curColor + '.magenta', (e) => e !== 'undefined' ?
+//                                                 parseInt(e) === 100 ? stopCheck() : null : null)
+//                                             : null
+//                                             : null)
+//                                             : null
+//                                         : null) :
+//                                         null :
+//                                     null
+//                             });
+//                         });
+//                     }
+//                 })
+//             })
+//         })
+//     });
+//
+//     setTimeout(() => document.getElementById('status').innerHTML = 'Все хорошо', 1000)
+// }
