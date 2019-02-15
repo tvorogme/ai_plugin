53919450b11812678739b96a4e46d3ccec2a9597
Григорий Вайсберг <gavaysberg@edu.hse.ru>
2019-02-15 14:54:34 +0300

    fix words change

--- js/main.js
+++ js/main.js
@@ -47,9 +47,6 @@ const toRegex = e => new RegExp(replaceAll(replaceAll(e, ' *', ' (\\S*){1}'), '*
 const fixBeforeRegex = (item) => [replaceAll(item, '\\', '\\\\'), ...regexFixChars].reduce(
     (item, fixItem) => replaceAll(item, fixItem));
 
-const words_to_replace = {'lol': 'kek'};
-const words_to_replace_keys = Object.keys(words_to_replace).map(fixBeforeRegex).map(toRegex);
-
 function mapOverChars(textFrameIndex, toFixLength, charIndex, toUpper = true) {
     const textFrameTemplate = `app.activeDocument.textFrames[${textFrameIndex}].textRange`;
 
@@ -75,67 +72,106 @@ function fixFonts(e) {
     downlodFile('https://raw.githubusercontent.com/tvorogme/ai_plugin/master/font_fix', (data) => {
         words_to_fix = data.split('\n').slice(0, -1).map(word => word.toLowerCase()).map(fixBeforeRegex).map(toRegex);
         downlodFile('https://raw.githubusercontent.com/tvorogme/ai_plugin/master/words_fix', (data_change) => {
-            console.log("data", data_change);
             words_to_change = data_change.split('\n').slice(0, -1).map((e, index) => words_to_change[index] = (e + '').split(';'));
             words_to_change.map((element, index) => {
-                //words_to_change[index].push(element[0].length);
+                words_to_change[index].push(element[0]);
                 words_to_change[index][0] = toRegex(fixBeforeRegex(element[0]));
                 words_to_change[index][1] = element[1];
             });
-            console.log(words_to_change);
             // достаем длину текст фреймов
             run('app.activeDocument.textFrames.length', (textRangeLength) => {
 
-            // для каждого фрейма
-            range(parseInt(textRangeLength)).map((textFrameIndex) => {
-                console.log(words_to_fix);
-                // посмотрим на текст
-                run(`app.activeDocument.textFrames[${textFrameIndex}].textRange.contents`, (text) => {
-                    // сравним его с тем, который нужно преобразовать
-                    const lowerText = text.toLowerCase();
-                    const founded_words = [].concat.apply([],
-                        words_to_fix.map(regex => lowerText.match(regex)));
+                // для каждого фрейма
+                range(parseInt(textRangeLength)).map((textFrameIndex) => {
 
-                    // для каждого слова
-                    founded_words.map((textToFix) => {
+                    // посмотрим на текст
+                    run(`app.activeDocument.textFrames[${textFrameIndex}].textRange.contents`, (text) => {
 
-                        // если оно есть в тексте
-                        if (lowerText.indexOf(textToFix) !== -1) {
-
-                            // пофиксим основное слово
-                            mapOverChars(textFrameIndex, textToFix.length, lowerText.indexOf(textToFix));
-
-                            // // Найдем где заканчивается последнее слово
-                            // const main_word_end = lowerText.indexOf(textToFix) + textToFix.length;
-                            //
-                            // // Найдем где заканчивается предложение
-                            // const text_end_on = lowerText.slice(
-                            //     lowerText.indexOf(textToFix) + textToFix.length).indexOf('.');
-                            //
-                            // // пофиксим все слова после основного слова
-                            // mapOverChars(textFrameIndex, text_end_on + 1, main_word_end)
-                        }
-                    });
-
+                        //достаём количество параграфов
                         run(`app.activeDocument.textFrames[${textFrameIndex}].paragraphs.length`, (paragraphsLength) => {
+
+                            //замена слов
+                            //бежим по параграфам
                             range(parseInt(paragraphsLength)).map((paragraphIndex) => {
+
+                                //достаём текст каждого параграфа
                                 run(`app.activeDocument.textFrames[${textFrameIndex}].paragraphs[${paragraphIndex}].contents`, (paragraphText) => {
-                                    console.log("paragraphIndex:", paragraphIndex, "text:", paragraphText);
+
+                                    let flag = false; //была ли сделана хотя бы 1 замена
+
+                                    //бежим по словам, котрые хотим заемнить
                                     words_to_change.map((regex) => {
-                                        console.log("regex:", regex);
-                                        paragraphText = paragraphText.replace(regex[0], (found) => {
-                                            if(regex[1].indexOf('*') !== -1){
-                                                let res = regex[1].split(" ");
-                                                let sfound = found.split(" ");
-                                                return res.map((elem, index) => res[index] = res[index] === "*" && index < sfound.length-1 ? sfound[index] : elem).join(" ");
-                                            }
-                                            return regex[1];
-                                        });
+
+                                        //проверка на то есть ли регексп в тексте
+                                        if (paragraphText.search(regex[0]) !== -1) {
+
+                                            //будет сделана замена
+                                            flag = true;
+                                            console.log(regex[2]);
+                                            //заменеям все регексы
+                                            paragraphText = paragraphText.replace(regex[0], (found) => {
+
+                                                //проверка на то, есть ли в регексе *
+                                                if (regex[1].indexOf('*') !== -1) {
+                                                    //если да то в финальную строку подставляем слова которые нужно оставить (работает плохо)
+                                                    let res = regex[1].split(" ");
+                                                    let replaces = [];
+                                                    let sfound = found.split(" ");
+
+                                                    regex[2].split(" ").map((elem, index) => {
+                                                        if(elem === "*")
+                                                            replaces.push(sfound[index]);
+                                                    });
+
+                                                    console.log(replaces);
+
+                                                    let starIter = 0;
+                                                    range(res.length).map((index) =>{
+                                                       if(res[index] === "*")
+                                                           res[index] = replaces[starIter++];
+                                                    });
+                                                    return res.join(" ");
+                                                }
+                                                //если нет то просто заменяем на нужную строку
+                                                return regex[1];
+                                            });
+                                        }
                                     });
-                                    run(`app.activeDocument.textFrames[${textFrameIndex}].paragraphs[${paragraphIndex}].contents="${paragraphText}"`);
+                                    //меняем текст только если была замена, чтобы лишний раз стили не портить
+                                    if (flag) {
+                                        run(`app.activeDocument.textFrames[${textFrameIndex}].paragraphs[${paragraphIndex}].contents="${paragraphText}"`);
+                                    }
                                 });
                             });
+                        });//конец замены слов
+
+                        // сравним его с тем, который нужно преобразовать
+                        const lowerText = text.toLowerCase();
+                        const founded_words = [].concat.apply([],
+                            words_to_fix.map(regex => lowerText.match(regex)));
+
+                        // для каждого слова
+                        founded_words.map((textToFix) => {
+
+                            // если оно есть в тексте
+                            if (lowerText.indexOf(textToFix) !== -1) {
+
+                                // пофиксим основное слово
+                                mapOverChars(textFrameIndex, textToFix.length, lowerText.indexOf(textToFix));
+
+                                // // Найдем где заканчивается последнее слово
+                                // const main_word_end = lowerText.indexOf(textToFix) + textToFix.length;
+                                //
+                                // // Найдем где заканчивается предложение
+                                // const text_end_on = lowerText.slice(
+                                //     lowerText.indexOf(textToFix) + textToFix.length).indexOf('.');
+                                //
+                                // // пофиксим все слова после основного слова
+                                // mapOverChars(textFrameIndex, text_end_on + 1, main_word_end)
+                            }
                         });
+
+
 
 
                         // const founded_words_to_replace = [].concat.apply([],
